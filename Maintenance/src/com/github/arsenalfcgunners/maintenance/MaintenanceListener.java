352c1aa81ff2946040827106425df7e5fa55841f
arsenalfcgunners <arsenalfcgunnersplaysmc@gmail.com>
2016-01-11 18:00:47 -0800

    Fixed an issue where a player was kicked in login rather than prelogin.

--- Maintenance/src/com/github/arsenalfcgunners/maintenance/MaintenanceListener.java
+++ Maintenance/src/com/github/arsenalfcgunners/maintenance/MaintenanceListener.java
@@ -2,12 +2,15 @@ package com.github.arsenalfcgunners.maintenance;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent.Result;
 import org.bukkit.event.server.ServerListPingEvent;
+
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 /**
  * 
@@ -16,6 +19,7 @@ import org.bukkit.event.server.ServerListPingEvent;
  * Listens for a player joining, or a server getting pinged.
  *
  */
+@SuppressWarnings("deprecation")
 public class MaintenanceListener implements Listener{
 	Maintenance maintenance;
 	String tag = ChatColor.GRAY+"["+ChatColor.GOLD+"GappleCraft"+ChatColor.GRAY+"] ";
@@ -29,33 +33,27 @@ public class MaintenanceListener implements Listener{
 	}
 	
 	/** 
-	 * Listens for a player joining and kicks them or sends them back to hub if the server is in maintenance mode and they do not have the permission "gapple.dev" or are whitelisted.
-	 * They are sent to the hub if the server is not the hub.  If the server is the hub they are kicked from the network.
+	 * When in maintenance mode, lets a player know that the server is in maintenance mode.
 	 * 
 	 * @param e The event.
 	 */
-	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.HIGH)
 	public void playerJoin(PlayerJoinEvent e){
-		final Player player = e.getPlayer();
-		if(maintenance.dev && !player.isWhitelisted() && !player.hasPermission("gapple.dev")){
-			if(maintenance.server != null && !maintenance.server.equalsIgnoreCase("hub")){
-				player.sendMessage(tag+ChatColor.RED+"The server is currently in "+ChatColor.YELLOW+"maintenance "+ChatColor.RED+"mode. We will be back soon. "+ChatColor.GRAY+"Check "+ChatColor.YELLOW+"www.gapplecraft.net"+ChatColor.GRAY+" for the latest updates.");
-				Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(maintenance, new Runnable() { // Plugin messages need delay in join event.
-		            public void run() {
-		            	if(maintenance.server == null){
-		            		maintenance.getMessagingChannel().getServerName(player);
-		            	}
-						maintenance.getMessagingChannel().connectToServer(player, "hub");
-		            }
-		        }, 2L);
-			}
-			else{
-				player.kickPlayer(tag+ChatColor.RED+"The server is currently in "+ChatColor.YELLOW+"maintenance "+ChatColor.RED+"mode. We will be back soon.\n"+ChatColor.GRAY+"Check "+ChatColor.YELLOW+"www.gapplecraft.net"+ChatColor.GRAY+" for the latest updates.");
-			}
+		if(maintenance.dev){
+			e.getPlayer().sendMessage(tag+ChatColor.RED+"The server is currently in "+ChatColor.YELLOW+"maintenance "+ChatColor.RED+"mode. Normal players cannot join during this time.");
 		}
-		else if(maintenance.dev){
-			player.sendMessage(tag+ChatColor.RED+"The server is currently in "+ChatColor.YELLOW+"maintenance "+ChatColor.RED+"mode. Normal players cannot join during this time.");
+	}
+	
+	/** 
+	 * Listens for a player joining and kicks them if they are not a dev, or are not whitelisted.
+	 * 
+	 * @param e The event.
+	 */
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onPreLogin(PlayerPreLoginEvent e){
+		String name = e.getName();
+		if(maintenance.dev && !PermissionsEx.getUser(e.getName()).has("gapple.dev") && !Bukkit.getOfflinePlayer(name).isWhitelisted()){
+			e.disallow(Result.KICK_OTHER, tag+ChatColor.RED+"The server is currently in "+ChatColor.YELLOW+"maintenance "+ChatColor.RED+"mode. We will be back soon. "+ChatColor.GRAY+"Check "+ChatColor.YELLOW+"www.gapplecraft.net"+ChatColor.GRAY+" for the latest updates.");
 		}
 	}
 	
